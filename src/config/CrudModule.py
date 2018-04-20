4f51856ab7e0c120dac9e280e0224a6c31bab32d
Ravi Adhikari <raviadhikary1996@gmail.com>
2018-04-20 12:36:39 +0545

    Naming Convention and Disable Logging

--- src/config/CrudModule.py
+++ src/config/CrudModule.py
@@ -4,13 +4,13 @@
 
 import sqlite3
 from sqlite3 import Error
-import logging
+# import logging
 
 '''
     Author: Ravi Adhikari
     Description: CRUD Module Designed facilitating Create, Read, Update and Delete
                  Unique Identifier Used: email
-                 User_CRUD
+                 UserCrud
                     |-__init__()
                     |-create() # Creates User Table
                     |-get_kwargs(**kwargs) #create a dictionary containing '' for values not provided by User
@@ -19,28 +19,29 @@ import logging
                     |-update() # Update fields based on matching criteria | email
                     |-delete() # Delete row value based on matching criteria | email
 '''
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-log_file_handler = logging.FileHandler('test.log')
-console_handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-log_file_handler.setLevel(logging.DEBUG)
-log_file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-console_handler.setFormatter(formatter)
-console_handler.setLevel(logging.ERROR)
-logger.addHandler(log_file_handler)
-logger.addHandler(console_handler)
+# logger = logging.getLogger()
+# logger.setLevel(logging.DEBUG)
+# log_file_handler = logging.FileHandler('test.log')
+# console_handler = logging.StreamHandler()
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# log_file_handler.setLevel(logging.DEBUG)
+# log_file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+# console_handler.setFormatter(formatter)
+# console_handler.setLevel(logging.ERROR)
+# logger.addHandler(log_file_handler)
+# logger.addHandler(console_handler)
 
 #
 ## Convert the methods to accept and modify a dynamic model
 #
 
-class User_CRUD:
-    def __init__(self):
-        self.conn_obj = sqlite3.connect('config/web.db')
-        self.cursor_obj = self.conn_obj.cursor()
-        self.user_values=['name', 'phone','email', 'bio',
+class UserCrud:
+    user_values=['name', 'phone','email', 'bio',
                           'dob', 'gender', 'address', 'lat', 'long', 'image', 'hyperlink']
+    def __init__(self):
+        self.conn_obj = sqlite3.connect('web.db')
+        self.cursor_obj = self.conn_obj.cursor()
+
     def create(self):
         create_statement="""CREATE TABLE IF NOT EXISTS User(
                             id INTEGER PRIMARY KEY,
@@ -60,23 +61,24 @@ class User_CRUD:
     def insert(self,**kwargs):
         for field in self.user_values:
             kwargs[field]=kwargs.get(field,'')
-        logger.info(kwargs)
+        # logger.info(kwargs)
         insert_statement="""
         INSERT INTO User (name, phone,email, bio, dob, gender, address, lat, long, image, hyperlink) 
         VALUES(:name, :phone, :email, :bio, :dob, :gender, :address, :lat, :long, :image, :hyperlink)"""
         resultset=self.cursor_obj.execute(insert_statement,kwargs)
-        if(resultset):
-            logger.info('Inserted Successfully')
+        # if(resultset):
+            # logger.info('Inserted Successfully')
         self.conn_obj.commit()
 
     def read(self,*selectvalues,**kwargs):
-        read_statement=' SELECT '+', '.join(selectvalues)+' FROM User '
+        sv=['*'] if selectvalues is () else selectvalues
+        print(sv)
+        read_statement='SELECT '+', '.join(sv)+' FROM User '
         selectives=[]
         for field, value in kwargs.items():
             selectives.append(str(str(field)+' = "'+str(value)+'"'))
         if selectives:
             read_statement+='WHERE '+' AND '.join(selectives)
-        print(read_statement)
         resultset = self.cursor_obj.execute(read_statement)
         return resultset
         
