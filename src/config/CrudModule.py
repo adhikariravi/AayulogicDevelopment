7aaee627c4f6eb10e51eb404b2b5d76bb8d348e3
Ravi Adhikari <raviadhikary1996@gmail.com>
2018-04-22 07:53:13 +0545

    Add pg_insert function to add into postgres. Differs from Sqlite

--- src/config/CrudModule.py
+++ src/config/CrudModule.py
@@ -3,72 +3,112 @@
 #
 
 import sqlite3
-from sqlite3 import Error
 
-from Connection import DatabaseConnection
+from config.Connection import DatabaseConnection
 
 '''
     Author: Ravi Adhikari
     Description: CRUD Module Designed facilitating Create, Read, Update and Delete
                  Unique Identifier Used: email
                  UserCrud
-                    |-__init__()
-                    |-create() # Creates User Table
-                    |-get_kwargs(**kwargs) #create a dictionary containing '' for values not provided by User
-                    |-insert(**kwargs) # Inserts rows into Database based on kwargs sent by user
-                    |-read(**kwargs) # Returns all matching records matching criteria
-                    |-update() # Update fields based on matching criteria | email
-                    |-delete() # Delete row value based on matching criteria | email
+                    |-__init__()    # Initialization 
+                    |-create()      # Creates User Table
+                    |-get_kwargs()  # Create a dictionary containing '' for values not provided by User
+                    |-insert()      # Inserts rows into Database based on kwargs sent by user
+                    |-read()        # Returns all matching records matching criteria
+                    |-update()      # Update fields based on matching criteria | email
+                    |-delete()      # Delete row value based on matching criteria | email
 '''
 #
 ## Convert the methods to accept and modify a dynamic model
 #
 
 class UserCrud:
-    user_values=['name', 'phone','email', 'bio',
-                          'dob', 'gender', 'address', 'lat', 'long', 'image', 'hyperlink']
+    user_values=['name', 'phone','email', 'bio', 'dob','gender',
+                 'address', 'lat', 'long', 'image', 'hyperlink']
+    
     def __init__(self):
-        self.conn_obj = sqlite3.connect('config/web.db')
-        self.cursor_obj = self.conn_obj.cursor()
+        # dbObject = DatabaseConnection('SQLITE')
+        dbObject=DatabaseConnection('POSTGRES')
+        self.conn_obj = dbObject.connection
+        self.cursor_obj = dbObject.cursor
 
     def create(self):
-        create_statement="""CREATE TABLE IF NOT EXISTS User(
-                            id INTEGER PRIMARY KEY,
+        # id INTEGER PRIMARY KEY,
+        create_statement="""CREATE TABLE IF NOT EXISTS Userinfo(
                             name VARCHAR(50) NOT NULL,
-                            phone VARCHAR(10) ,
+                            phone VARCHAR(50) ,
                             email VARCHAR(100),
                             bio TEXT,
                             dob DATE,
-                            gender INT NOT NULL,
+                            gender CHAR(1) ,
                             address VARCHAR(200),
                             lat VARCHAR(100),
                             long VARCHAR(100),
-                            image BLOB,
+                            image VARCHAR(100),
                             hyperlink TEXT)"""
         self.cursor_obj.execute(create_statement)
+        self.conn_obj.commit()
     
     def insert(self,**kwargs):
         for field in self.user_values:
             kwargs[field]=kwargs.get(field,'')
         insert_statement="""
-        INSERT INTO User (name, phone,email, bio, dob, gender, address, lat, long, image, hyperlink) 
+        INSERT INTO Userinfo (name, phone,email, bio, dob, gender, address, lat, long, image, hyperlink) 
         VALUES(:name, :phone, :email, :bio, :dob, :gender, :address, :lat, :long, :image, :hyperlink)"""
         self.cursor_obj.execute(insert_statement,kwargs)
         self.conn_obj.commit()
+    
+    @staticmethod
+    def quote(field):
+        return "'"+field+"'" if type(field) is str else str(field)
+
+    def pg_insert(self,values,counter):
+        #
+        ## Skipping Field values to directly supply id
+        #
+        ins='INSERT INTO Userinfo(name, phone,email, bio, dob, gender, address, lat, long, image, hyperlink) values ('
+        insertees=list()
+        for each_value in values:
+            insertees.append(self.quote(each_value))
+        final_stmt=ins+','.join(insertees)+')'
+        self.cursor_obj.execute(final_stmt)
+        if(int(counter%100)==0):
+            self.conn_obj.commit()
+        
+
+
+    # @staticmethod
+    # def get_kwargs(**kwargs):
+    #     for field in self.user_values:
+    #         kwargs[field]=kwargs.get(field,'')
+    #     return kwargs
+
+    def batch_insert(self,dictionary_list):
+        #
+        ## Considering the batch insert is automated using a module
+        ## We will not check if all the values in kwargs are set.
+        #
+        multi_insert_stmt="""
+        INSERT INTO Userinfo (name, phone,email, bio, dob, gender, address, lat, long, image, hyperlink) 
+        VALUES(:name, :phone, :email, :bio, :dob, :gender, :address, :lat, :long, :image, :hyperlink)"""
+        self.cursor_obj.executemany(multi_insert_stmt,dictionary_list)
+        self.conn_obj.commit()
+
 
     def read(self,*selectvalues,**kwargs):
         sv=['*'] if selectvalues is () else selectvalues
-        read_statement='SELECT '+', '.join(sv)+' FROM User'
+        read_statement='SELECT '+', '.join(sv)+' FROM Userinfo'
         selectives=[]
         for field, value in kwargs.items():
-            selectives.append(str(str(field)+' = "'+str(value)+'"'))
+            selectives.append(str(str(field)+" = '"+str(value)+"'"))
         if selectives:
             read_statement+=' WHERE '+' AND '.join(selectives)
-        resultset = self.cursor_obj.execute(read_statement).fetchall()
-        return resultset
+        self.cursor_obj.execute(read_statement)
+        return self.cursor_obj.fetchall()
         
     def update(self,email_criteria, **uservalues):
-        update_stmt = "UPDATE User SET"
+        update_stmt = "UPDATE Userinfo SET"
         updates=[]
         for keys,values in uservalues.items():
             updates.append(' '+keys+' = "'+values+'"')
@@ -76,5 +116,5 @@ class UserCrud:
         self.cursor_obj.execute(update_stmt)
 
     def delete(self,email_criteria):
-        delete_statement='DELETE FROM User WHERE email="'+email_criteria+'"'
+        delete_statement='DELETE FROM Userinfo WHERE email="'+email_criteria+'"'
         self.cursor_obj.execute(delete_statement)
